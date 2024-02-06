78a3d995e160e76fa9ebffa478a6ded3e18641ce
Zee022021 <haqyarzia@gmail.com>
2024-02-05 19:19:49 -0800

    Python if elif and else statements

---
+++ basic_python/if_statements.py
@@ -0,0 +1,15 @@
+a = 100
+b = 200
+c = 400
+if a > b:
+    print('a is greater than b')
+elif b < a:
+    print('b is less than a')
+elif b > c:
+    print('b is greater than c')
+elif c < b:
+    print('c is less than b')
+elif c > a:
+    print('c is greater than a c is greater than a and b')
+else:
+    print('c is greater than a and b')
