4b047cb59a33615431dcce190f60181caa872196
Zee022021 <haqyarzia@gmail.com>
2024-02-05 19:16:35 -0800

    Python Dictionaries Commit

---
+++ basic_python/dictionaries.py
@@ -0,0 +1,2 @@
+csa_courses={"1":"Systems", "2":"Networks", "3":"GRC"}
+osi_layers = {"Layer 1":"Physical", "Layer 2":"Data", "Layer 3":"Network", "Layer 4":"Transport", "Layer 5": "Session", "Layer 6": "Presentation", "Layer 7":"Application"}
