c7fbf88b0974d60bfbcc1bde1e3b74a6d84e13f8
Zee022021 <haqyarzia@gmail.com>
2024-02-12 05:26:07 -0800

    guessing the world game commit

---
+++ basic_python/guessing_the_world_game.py
@@ -0,0 +1,27 @@
+import random
+the_number = random.randint(1,100)
+
+guess = 0
+
+while guess != the_number:
+    guess = int(input("Please guess the number: ")) 
+
+    if guess > the_number:
+        print("Player Guess Lower. . . \n")
+    elif guess < the_number:
+        print("Player Guess Higher. . . \n")
+    else:
+        print(f"Game Over! The number was {the_number}. The Player Wins!\n")
+        break
+
+    guess = random.randint(1,100)
+    if guess > the_number:
+        print("Computer Guess Lower. . . \n")
+    elif guess < the_number:
+        print("Computer Guess Higher. . . \n")
+    else:
+        print(f"Game Over! The number was {the_number}. The Computer Wins!\n")
+        break
+
+
+next = input("Press Enter to continue....")
