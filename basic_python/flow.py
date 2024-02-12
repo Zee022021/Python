2f282c81d009d72b463d177aaaeb7df0db62984e
Zee022021 <haqyarzia@gmail.com>
2024-02-12 05:28:17 -0800

    passing a list as an aurguement

---
+++ basic_python/flow.py
@@ -0,0 +1,10 @@
+#pass a list as an argument
+
+def new_function(computer):
+    for x in computer:
+        print(x)
+requirements = ["Core i9", "16GB RAM", "Windows 10 Pro"]
+
+new_function(requirements)
+x = 44 % 5
+print(x)
